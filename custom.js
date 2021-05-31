c10ef7007f5547395a7aa93b4d1b0aa2c1333f75
Gacheri <michenibrenda81@gmail.com>
2021-05-31 16:31:50 +0300

    toggle-start-button

--- custom.js
+++ custom.js
@@ -2,3 +2,14 @@ var date = new Date();
 var today = date.getHours()+ ":" + date.getMinutes();
 document.getElementById('endtime').innerHTML = today;
 
+const start = document.getElementById("start-menu");
+const btn = document.getElementById("start-button");
+
+btn.onclick = function(){
+   if(start.style.display === "none"){
+    start.style.display = "block";
+}else{
+    start.style.display = "none"
+}
+}
+
